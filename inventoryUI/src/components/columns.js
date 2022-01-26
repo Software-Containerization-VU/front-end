8b09f3fea37cd2782a846f6c9104e003404b3fc5
lkrumpak <l.krumpak@student.vu.nl>
2022-01-26 18:30:27 +0100

    restructuring

---
+++ inventoryUI/src/components/columns.js
@@ -0,0 +1,18 @@
+export const COLUMNS = [
+    {
+        Header: 'id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Product',
+        accessor: 'product'
+    },
+    {
+        Header: 'Quantity',
+        accessor: 'quantity'
+    },
+    {
+        Header: 'Date Added',
+        accessor: 'date_added'
+    }
+]
