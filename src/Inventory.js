bedf9ec6606da766f2cc617fa0e1477b73e8dbbf
lkrumpak <l.krumpak@student.vu.nl>
2022-01-28 12:49:17 +0100

    it should just work

--- src/Inventory.js
+++ src/Inventory.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import image from './imgs/warehouses.png'
+import axios from 'axios'
 
 export default function Inventory() {
 
-    const [items, setItems] = useState()
+    const [items, setItems] = useState([])
     const [name, setName] = useState('')
-    const [quantity, setQuantity] = useState()
+    const [price, setPrice] = useState(0)
 
     const getInventory = () => {
-        Axios.get("http://10.152.183.25:8081/inventory/api/v1.0/books").then((res) => {
-            console.log(res.data.books)
-            setItems(res.data.books)
+        Axios.get("http://10.64.140.43:8090/api/product").then((res) => {
+            setItems(res.data)
         })
     }
 
-    const addItem = (e) => {
-        e.preventDefault()
-        // post request
-        console.log(name)
-        console.log(quantity)
+    const addItem = () => {
+        const post = {"productName": name, "productPrice": price}
+        
+        axios.post("http://10.64.140.43:8090/api/product", post).then((res) => {
+            getInventory()
+        }).catch((error) => {
+            console.log(error)
+        })
+
         setName('')
-        setQuantity('')
+        setPrice(0)
+    }
+
+    const deleteItem = (id) => {
+        axios.delete(`http://10.64.140.43:8090/api/product/${id}`).then((res) => {
+            getInventory()
+        }).catch((error) => {
+            console.log(error)
+        })
     }
 
     useEffect(() => {
@@ -32,40 +44,40 @@ export default function Inventory() {
     <img src={image} alt='-' className='w-20 h-20 mx-auto'></img>
     <h1 className='text-4xl mb-8'>Inventory Management</h1>
     <div className='bg-white w-1/2 mx-auto'>
-        <form onSubmit={(e) => addItem(e)}>
+        
         <table className='w-full drop-shadow-md'>
             <thead className='bg-gray-50 border-b-2 border-gray-200'>
                 <tr>
                     <th className='p-3 text-sm font-semibold tracking-wide text-left'>#</th>
                     <th className='p-3 text-sm font-semibold tracking-wide text-left'>Item</th>
-                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Quantity</th>
+                    <th className='p-3 text-sm font-semibold tracking-wide text-left'>Price</th>
                     <th className='p-3 text-sm font-semibold tracking-wide text-left'>Action</th>
                 </tr>
             </thead>
             <tbody>
                 {items ? items.map((item) => 
-                    <tr className='bg-white' key={item.id}>
-                        <td className='p-3 text-sm text-blue-500 text-left font-bold'>{item.id}</td>
-                        <td className='p-3 text-sm text-gray-700 text-left'>{item.title}</td>
-                        <td className='p-3 text-sm text-gray-700 text-left'>{item.quantity}</td>
-                        <td className='p-3 text-sm text-gray-700 text-left'><button>Remove</button></td>
+                    <tr className='bg-white' key={item.productId}>
+                        <td className='p-3 text-sm text-blue-500 text-left font-bold'>{item.productId}</td>
+                        <td className='p-3 text-sm text-gray-700 text-left'>{item.productName}</td>
+                        <td className='p-3 text-sm text-gray-700 text-left'>{item.productPrice}</td>
+                        <td className='p-3 text-sm text-gray-700 text-left'><button onClick={() => deleteItem(item.productId)}>Remove</button></td>
                     </tr>
                 ) : null}
-                <tr className='bg-white'>
+                <tr className='bg-white' key="add">
                     <td className='p-3 text-sm text-blue-500 text-left font-bold'>#</td>
                     <td className='p-3 text-sm text-gray-700 text-left'>
-                        <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='Name of Product' required></input>
+                        <input type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='E.g. White Shirt' required></input>
                     </td>
                     <td className='p-3 text-sm text-gray-700 text-left'>
-                        <input type='text' value={quantity} onChange={(e) => setQuantity(e.target.value)} placeholder='Quantity' required></input>
+                        <input type='number' value={price} onChange={(e) => setPrice(e.target.value)} placeholder='12' required></input>
                     </td>
                     <td className='p-3 text-sm text-gray-700 text-left'>
-                        <button type='submit'>Add</button>
+                        <button onClick={() => addItem()}>Add</button>
                     </td>
                 </tr>
             </tbody>
         </table>
-        </form>
+       
     </div>
     </>;
 }
